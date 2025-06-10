83db6e5c507998aba7de547ab09b339a890fe1dc
chouno0507 <chouno2008@gmail.com>
2025-06-10 15:59:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("📊 학력·기업규모별 근로조건 비교 분석")
-st.image("http://i0.wp.com/seoulcity.kr/wp-content/uploads/2025/03/2503072.png?w=1051&ssl=1")
+st.image("250372.png")
 # 파일 업로더
 uploaded_file = st.file_uploader("📁 엑셀 파일을 업로드하세요", type=["xlsx"])
 
